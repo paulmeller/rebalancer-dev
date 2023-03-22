9d4ea7c000d8699aa7559e288f4851b30835d9d4
Paul Meller <14568+paulmeller@users.noreply.github.com>
2023-03-22 16:31:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def display_editable_table(df, title):
 
 # Define a function to plot the portfolio allocation
 def plot_allocation(portfolio, prices):
-    portfolio_values = {stock: (prices[stock] * (weight for stock, weight in portfolio.items()))}
+    portfolio_values = {stock: prices[stock] * weight for stock, weight in portfolio.items()}
     fig = px.pie(names=list(portfolio.keys()), values=list(portfolio_values.values()))
     st.plotly_chart(fig)
 
