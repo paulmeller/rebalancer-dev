2d0c74928f01888102af925cb04c388637daf217
Paul Meller <14568+paulmeller@users.noreply.github.com>
2023-03-22 16:23:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,10 +63,10 @@ df_holdings['Value'] = df_holdings['Price'] * df_holdings['Shares']
 current_portfolio_value = df_holdings['Value'].sum()
 
 # Define the initial portfolio weights as a dataframe
-df_weights = pd.DataFrame({'Ticker': ['AAPL', 'MSFT', 'GOOG'], 'Weight': [0.3, 0.5, 0.2]}).set_index('Ticker')
+df_target_weights = pd.DataFrame({'Ticker': ['AAPL', 'MSFT', 'GOOG'], 'Weight': [0.3, 0.5, 0.2]}).set_index('Ticker')
 
 # Ask the user for their target allocation
-df_target_weights = display_editable_table(df_weights, 'Target Weights')
+# df_target_weights = display_editable_table(df_weights, 'Target Weights')
 
 # Calculate the target portfolio value
 target_portfolio_value = current_portfolio_value * (df_target_weights['Weight'].sum() / df_weights['Weight'].sum())
