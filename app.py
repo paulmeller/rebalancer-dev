7134f68f56a006f283803055beb2a8209a8d2f04
Paul Meller <14568+paulmeller@users.noreply.github.com>
2023-03-24 01:52:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,46 @@
-# Import required libraries
+import yfinance as yf
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
 
-# Function to calculate monthly mortgage payment
-def calculate_mortgage_payment(principal, annual_interest_rate, term_years):
-    monthly_interest_rate = annual_interest_rate / 12 / 100
-    num_payments = term_years * 12
-    monthly_payment = principal * (monthly_interest_rate * np.power(1 + monthly_interest_rate, num_payments)) / (np.power(1 + monthly_interest_rate, num_payments) - 1)
-    return monthly_payment
+# Define function to get stock data and calculate trades
+def rebalance_portfolio(portfolio):
+    # Get stock data from yfinance
+    data = yf.download(portfolio.keys())
+    # Calculate current portfolio value
+    current_value = (data['Adj Close'] * portfolio.values()).sum()
+    # Calculate target portfolio value
+    target_value = current_value.sum()
+    target_weights = current_value / target_value
+    # Calculate trades required to rebalance portfolio
+    trades = target_weights - portfolio.values()
+    trades *= target_value / data['Adj Close'].iloc[-1]
+    trades = trades.round(0).astype(int)
+    # Create summary table of trades
+    summary = pd.DataFrame({'Ticker': portfolio.index, 'Target Weight': target_weights, 'Current Weight': portfolio.values(), 'Shares': trades})
+    summary = summary[summary['Shares'] != 0]
+    return summary
 
-# Function to generate mortgage payment breakdown pie chart
-def create_payment_breakdown_chart(principal, interest_rate, mortgage_term):
-    plt.figure(figsize=(6, 6))
-    labels = ['Principal', 'Interest']
-    sizes = [principal, (interest_rate * mortgage_term * principal / 100)]
-    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-    plt.axis('equal')
-    plt.title('Mortgage Payment Breakdown')
-    return plt
+# Define function to display summary table
+def display_summary(summary):
+    st.write('## Summary of Trades')
+    st.write(summary)
 
-# Function to generate remaining balance bar chart
-def create_remaining_balance_chart(principal, interest_rate, mortgage_term):
-    num_years = np.arange(1, mortgage_term + 1)
-    remaining_balances = [principal * (1 - (np.power(1 + interest_rate / 100, i) - 1) / np.power(1 + interest_rate / 100, mortgage_term)) for i in num_years]
+# Define main function
+def main():
+    # Set page title
+    st.set_page_config(page_title='Portfolio Rebalancer')
+    # Set page header
+    st.write('# Portfolio Rebalancer')
+    # Get user input for portfolio
+    st.write('## Input Portfolio')
+    tickers = st.text_input('Enter ticker symbols separated by commas (e.g., AAPL, GOOG)', 'AAPL,GOOG')
+    weights = st.text_input('Enter target weights separated by commas (e.g., 0.5, 0.5)', '0.5,0.5')
+    tickers = [ticker.strip().upper() for ticker in tickers.split(',')]
+    weights = [float(weight.strip()) for weight in weights.split(',')]
+    portfolio = pd.Series(index=tickers, data=weights)
+    # Rebalance portfolio and display summary table
+    summary = rebalance_portfolio(portfolio)
+    display_summary(summary)
 
-    plt.figure(figsize=(10, 6))
-    plt.bar(num_years, remaining_balances)
-    plt.xlabel('Year')
-    plt.ylabel('Remaining Balance ($)')
-    plt.title('Remaining Mortgage Balance Over Time')
-    plt.xticks(num_years)
-    return plt
-
-# App title and description
-st.title("Mortgage Calculator")
-st.write("Estimate your monthly mortgage payment based on the loan amount, interest rate, and term.")
-
-# User inputs
-home_price = st.number_input("Home Price", value=250000, step=1000, format="%i")
-down_payment = st.number_input("Down Payment", value=50000, step=1000, format="%i")
-mortgage_term = st.slider("Mortgage Term (Years)", min_value=10, max_value=30, value=30, step=1)
-interest_rate = st.slider("Interest Rate (%)", min_value=0.0, max_value=10.0, value=3.5, step=0.1)
-
-# Calculate mortgage principal and monthly payment
-principal = home_price - down_payment
-monthly_payment = calculate_mortgage_payment(principal, interest_rate, mortgage_term)
-
-# Display the results
-st.write(f"Loan Amount: ${principal:,.2f}")
-st.write(f"Monthly Mortgage Payment: ${monthly_payment:,.2f}")
-
-# Create and display visualizations
-st.pyplot(create_payment_breakdown_chart(principal, interest_rate, mortgage_term))
-st.pyplot(create_remaining_balance_chart(principal, interest_rate, mortgage_term))
+# Call main function
+if __name__ == '__main__':
+    main()
